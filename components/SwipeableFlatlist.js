99e03ccb5dbe3850f0ed05a509a39fb98554e0e4
vishalgaddam873 <vishal18@navgurukul.org>
2021-01-07 14:33:23 +0530

    Some changes

--- components/SwipeableFlatlist.js
+++ components/SwipeableFlatlist.js
@@ -1,116 +1,115 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Animated,
-    Dimensions,
-    StyleSheet,
-    Text,
-    TouchableHighlight,
-    View,
-} from 'react-native';
-import { ListItem, Icon } from 'react-native-elements';
+  Animated,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from "react-native";
+import { ListItem, Icon } from "react-native-elements";
 
-import { SwipeListView } from 'react-native-swipe-list-view';
+import { SwipeListView } from "react-native-swipe-list-view";
 
-import db from '../config';
+import db from "../config";
 
-
-export default class SwipeableFlatlist extends Component{
+export default class SwipeableFlatlist extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allNotifications : this.props.allNotifications,
+      allNotifications: this.props.allNotifications
     };
   }
 
-
-  updateMarkAsread =(notification)=>{
-    db.collection("all_notifications").doc(notification.doc_id).update({
-      "notification_status" : "read"
-    })
-  }
+  updateMarkAsread = notification => {
+    db.collection("all_notifications")
+      .doc(notification.doc_id)
+      .update({
+        notification_status: "read"
+      });
+  };
 
   onSwipeValueChange = swipeData => {
-    var allNotifications = this.state.allNotifications
-      const {key,value} = swipeData;
-
-      if(value < -Dimensions.get('window').width){
-   const newData = [...allNotifications];
-       const prevIndex = allNotifications.findIndex(item => item.key === key);
-       this.updateMarkAsread(allNotifications[prevIndex]);
-         newData.splice(prevIndex, 1);
-        this.setState({allNotifications : newData})
-   };
-};
+    var allNotifications = this.state.allNotifications;
+    const { key, value } = swipeData;
+    if (value < -Dimensions.get("window").width) {
+      const newData = [...allNotifications];
+      this.updateMarkAsread(allNotifications[key]);
+      newData.splice(key, 1);
+      this.setState({ allNotifications: newData });
+    }
+  };
 
   renderItem = data => (
-      <Animated.View>
-        <ListItem
-          leftElement={<Icon name="book" type="font-awesome" color ='#696969'/>}
-          title={data.item.book_name}
-          titleStyle={{ color: 'black', fontWeight: 'bold' }}
-          subtitle={data.item.message}
-          bottomDivider
-        />
-      </Animated.View>
+    <Animated.View>
+      <ListItem
+        leftElement={<Icon name="book" type="font-awesome" color="#696969" />}
+        title={data.item.book_name}
+        titleStyle={{ color: "black", fontWeight: "bold" }}
+        subtitle={data.item.message}
+        bottomDivider
+      />
+    </Animated.View>
   );
 
   renderHiddenItem = () => (
-      <View style={styles.rowBack}>
-          <View style={[styles.backRightBtn, styles.backRightBtnRight]}>
-              <Text style={styles.backTextWhite}></Text>
-          </View>
+    <View style={styles.rowBack}>
+      <View style={[styles.backRightBtn, styles.backRightBtnRight]}>
+        <Text style={styles.backTextWhite}>Mark as read</Text>
       </View>
+    </View>
   );
 
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.container}>
-          <SwipeListView
-              disableRightSwipe
-              data={this.state.allNotifications}
-              renderItem={this.renderItem}
-              renderHiddenItem={this.renderHiddenItem}
-              rightOpenValue={-Dimensions.get('window').width}
-              previewRowKey={'0'}
-              previewOpenValue={-40}
-              previewOpenDelay={3000}
-              onSwipeValueChange={this.onSwipeValueChange}
-          />
+        <SwipeListView
+          disableRightSwipe
+          data={this.state.allNotifications}
+          renderItem={this.renderItem}
+          renderHiddenItem={this.renderHiddenItem}
+          rightOpenValue={-Dimensions.get("window").width}
+          previewRowKey={"0"}
+          previewOpenValue={-40}
+          previewOpenDelay={3000}
+          onSwipeValueChange={this.onSwipeValueChange}
+          keyExtractor={(item, index) => index.toString()}
+        />
       </View>
-    )
+    );
   }
-
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        flex: 1,
-    },
-    backTextWhite: {
-        color: '#FFF',
-        fontWeight:'bold',
-        fontSize:15
-    },
-    rowBack: {
-        alignItems: 'center',
-        backgroundColor: '#29b6f6',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingLeft: 15,
-    },
-    backRightBtn: {
-        alignItems: 'center',
-        bottom: 0,
-        justifyContent: 'center',
-        position: 'absolute',
-        top: 0,
-        width: 100,
-    },
-    backRightBtnRight: {
-        backgroundColor: '#29b6f6',
-        right: 0,
-    },
+  container: {
+    backgroundColor: "white",
+    flex: 1
+  },
+  backTextWhite: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 15,
+    textAlign: "center",
+    alignSelf: "flex-start"
+  },
+  rowBack: {
+    alignItems: "center",
+    backgroundColor: "#29b6f6",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 15
+  },
+  backRightBtn: {
+    alignItems: "center",
+    bottom: 0,
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    width: 100
+  },
+  backRightBtnRight: {
+    backgroundColor: "#29b6f6",
+    right: 0
+  }
 });
--- config.js
+++ config.js
@@ -1,17 +1,16 @@
-import firebase from 'firebase';
-require('@firebase/firestore')
+import firebase from "firebase";
+require("@firebase/firestore");
 
 var firebaseConfig = {
-    apiKey: "AIzaSyASyOjOtJla-X-b3io2eLoaUc_bIRFSIIc",
-    authDomain: "book-santa-app.firebaseapp.com",
-    databaseURL: "https://book-santa-app.firebaseio.com",
-    projectId: "book-santa-app",
-    storageBucket: "book-santa-app.appspot.com",
-    messagingSenderId: "69634746716",
-    appId: "1:69634746716:web:6fbbfc110fb4475365f999",
-    measurementId: "G-DLB7XC0JPL"
-  };
-
+  apiKey: "AIzaSyBJsTqwL8yjTWW8mrCWgrBird7HI944SIw",
+  authDomain: "book-santa.firebaseapp.com",
+  databaseURL: "https://book-santa.firebaseio.com",
+  projectId: "book-santa",
+  storageBucket: "book-santa.appspot.com",
+  messagingSenderId: "546422247592",
+  appId: "1:546422247592:web:a82af183e05f978755f29d",
+  measurementId: "G-MLCQNSGDLH"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
